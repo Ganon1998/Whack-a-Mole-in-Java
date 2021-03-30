95a2434d026abdcb96eceb2b7def83c499deeca5
delaneyy996 <81336773+delaneyy996@users.noreply.github.com>
2021-03-30 19:58:49 -0400

    Update GameBoard.java

--- GameBoard.java
+++ GameBoard.java
@@ -12,178 +12,298 @@ public class GameBoard extends JFrame {
     private static JLabel[] holeDesgin = new JLabel[9];
     public GameBoard() {
 
-    setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-    setBounds(150,150,660,770); // set frame bounds
-    setTitle("WHACK-A-MOLE");
-    setVisible( true ); // display frame
-    //set menu panel
-    JPanel startMenu = new JPanel();
+        setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        setBounds(150,150,660,770); // set frame bounds
+        setTitle("WHACK-A-MOLE");
+        setVisible( true ); // display frame
+        //set menu panel
+        JPanel startMenu = new JPanel();
 
-    //set buttons
-    JButton startGame = new JButton("Start");
-    JButton rules = new JButton("Rules");
-    JButton leaderboard = new JButton("Leaderboard");
-    JButton quit = new JButton("Quit");
+        //set buttons
+        JButton startGame = new JButton("Start");
+        JButton rules = new JButton("Rules");
+        JButton leaderboard = new JButton("Leaderboard");
+        JButton quit = new JButton("Quit");
 
-    //create starting menu, this button will act as the "go to main menu button"
-    JButton toStart = new JButton("Main Menu");
-    startMenu.setBackground(new Color(0,0,0));
-    startMenu.add(toStart);
-    setContentPane(startMenu);
-
-    //go to main menu when button pressed
-    toStart.addActionListener(new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-
-            //repaint panel
-            startMenu.removeAll();
-            revalidate();
-            repaint();
-
-            startMenu.setLayout(new BoxLayout(startMenu,BoxLayout.PAGE_AXIS));
-            startMenu.setBackground(new Color(65,45,20));
-
-            //set title header
-            Font headFont = new Font(Font.SANS_SERIF,Font.BOLD,50);
-            JLabel header = new JLabel("Whack-A-Mole!");
-            header.setForeground(new Color (0,0,0));
-            header.setFont(headFont);
-            add(header);
-            header.setAlignmentX(Component.CENTER_ALIGNMENT);
+        //create starting menu, this button will act as the "go to main menu button"
+        JButton toStart = new JButton("Main Menu");
+        startMenu.setBackground(new Color(0,0,0));
+        startMenu.add(toStart);
+        setContentPane(startMenu);
 
 
-            //create an invisible components to space out the buttons
-            Dimension minSize = new Dimension(10,50);
-            Dimension prefSize = new Dimension(10,50);
-            Dimension maxSize = new Dimension(Short.MAX_VALUE,100);
+        //this class draws the ovals around the holeDesign jpanels
+        class DrawGameBoardHoles extends JPanel{
+            @Override
+            public void paintComponent(Graphics g) {
 
-            add(new Box.Filler(minSize,prefSize,maxSize));
-            add(startGame);
-            add(new Box.Filler(minSize,prefSize,maxSize));
-            add(rules);
-            add(new Box.Filler(minSize,prefSize,maxSize));
-            add(leaderboard);
-            add(new Box.Filler(minSize,prefSize,maxSize));
-            add(quit);
 
-            setContentPane(startMenu);
-        }
-    });
+                super.paintComponent(g);
+                g.setColor(Color.RED);
+                g.drawOval(50,150,100,100);
+                g.drawOval(250,150,100,100);
+                g.drawOval(450,150,100,100);
+                g.drawOval(50,300,100,100);
+                g.drawOval(250,300,100,100);
+                g.drawOval(450,300,100,100);
+                g.drawOval(50,450,100,100);
+                g.drawOval(250,450,100,100);
+                g.drawOval(450,450,100,100);
 
-    //Start game, this will draw the board
-    startGame.addActionListener(new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            //repaint panel
-            startMenu.removeAll();
-            revalidate();
-            repaint();
-            JLabel hi = new JLabel("hi");
-            startMenu.setBackground(new Color(65,45,20));
 
-            for (int i=0; i<9; i++){
-                holeDesgin[i] = new JLabel("HOLE" + i);
-                holeDesgin[i].setName("HOLE" + i);
-                if(i==0){
-                    holeDesgin[i].setBounds(0,300,150,150);
-                }
-                add(holeDesgin[i]);
+
+
+                setBackground(new Color(20,80,0));
+
+
+
             }
-
-            // mouse action
-            ClickGameBoard clicker = new ClickGameBoard();
-
-
-            setContentPane(startMenu);
-
         }
-    });
 
 
-    rules.addActionListener(new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-
-            //repaint screen
-            startMenu.removeAll();
-            revalidate();
-            repaint();
-
-            //set invisible spacing dimensions
-            Dimension minSize = new Dimension(10,600);
-            Dimension prefSize = new Dimension(10,600);
-            Dimension maxSize = new Dimension(Short.MAX_VALUE,600);
 
 
-            startMenu.setBackground(new Color(2,45,20));
+        //go to main menu when button pressed
+        toStart.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
 
-            //set header text
-            Font headFont = new Font(Font.SANS_SERIF,Font.BOLD,50);
-            JLabel header = new JLabel("Rules");
-            header.setForeground(new Color (0,0,0));
-            header.setFont(headFont);
-            add(header);
+                //repaint panel
+                startMenu.removeAll();
+                revalidate();
+                repaint();
 
-            //////ADD TEXT HERE TO EXPLAIN RULES ETC////////
+                startMenu.setLayout(new BoxLayout(startMenu,BoxLayout.PAGE_AXIS));
+                startMenu.setBackground(new Color(65,45,20));
+
+                //set title header
+                Font headFont = new Font(Font.SANS_SERIF,Font.BOLD,50);
+                JLabel header = new JLabel("Whack-A-Mole!");
+                header.setForeground(new Color (0,0,0));
+                header.setFont(headFont);
+                add(header);
+                header.setAlignmentX(Component.CENTER_ALIGNMENT);
 
 
-            //add button
-            add(new Box.Filler(minSize,prefSize,maxSize));
-            add(toStart);
-            toStart.setAlignmentX(Component.CENTER_ALIGNMENT);
-            header.setAlignmentX(Component.CENTER_ALIGNMENT);
-            setContentPane(startMenu);
-        }
-    });
+                //create an invisible components to space out the buttons
+                Dimension minSize = new Dimension(10,50);
+                Dimension prefSize = new Dimension(10,50);
+                Dimension maxSize = new Dimension(Short.MAX_VALUE,100);
 
-    leaderboard.addActionListener(new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            //repaint screen
-            startMenu.removeAll();
-            revalidate();
-            repaint();
+                add(new Box.Filler(minSize,prefSize,maxSize));
+                add(startGame);
+                add(new Box.Filler(minSize,prefSize,maxSize));
+                add(rules);
+                add(new Box.Filler(minSize,prefSize,maxSize));
+                add(leaderboard);
+                add(new Box.Filler(minSize,prefSize,maxSize));
+                add(quit);
 
-            //set invisible spacing dimensions
-            Dimension minSize = new Dimension(10,600);
-            Dimension prefSize = new Dimension(10,600);
-            Dimension maxSize = new Dimension(Short.MAX_VALUE,600);
+                setContentPane(startMenu);
+            }
+        });
 
-            startMenu.setBackground(new Color(100,0,0));
+        //Start game, this will draw the board
+        startGame.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //repaint panel
+                startMenu.removeAll();
+                revalidate();
+                repaint();
 
-            //set header text
-            Font headFont = new Font(Font.SANS_SERIF,Font.BOLD,50);
-            JLabel header = new JLabel("Leaderboard");
-            header.setForeground(new Color (0,0,0));
-            header.setFont(headFont);
-            add(header);
+                //calls the class DrawGameBoardHoles to create the hole drawings
+                DrawGameBoardHoles draw = new DrawGameBoardHoles();
+                startMenu.add(draw);
 
-            //add button
-            add(new Box.Filler(minSize,prefSize,maxSize));
-            add(toStart);
-            toStart.setAlignmentX(Component.CENTER_ALIGNMENT);
-            header.setAlignmentX(Component.CENTER_ALIGNMENT);
-            setContentPane(startMenu);
-        }
-    });
+                //set the header font and text
+                Font headFont = new Font(Font.SANS_SERIF,Font.BOLD,30);
+                JLabel header = new JLabel("Whack The Moles!!");
+                header.setForeground(new Color (0,0,0));
+                header.setFont(headFont);
+                add(header);
+               // header.setAlignmentX(Component.TOP_ALIGNMENT);
 
-    quit.addActionListener(new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
+                Font font = new Font (Font.DIALOG, Font.BOLD, 20);
 
-            System.exit(0);
-        }
-    });
-}
+                JLabel scoreText = new JLabel("Score: ");
+                JLabel timerText = new JLabel("Time: ");
+                JLabel secondsText = new JLabel("20");
+                JLabel scoreNumText = new JLabel("0");
+                
+                /*for (int i=30; i>0; i--){
+                    secondsText = new JLabel(String.valueOf(i));
+                    try {
+                        Thread.sleep(1000L);
+                    } catch (InterruptedException interruptedException) {
+                        interruptedException.printStackTrace();
+                    }
+                }*/
+                scoreText.setForeground(Color.RED);
+                timerText.setForeground(Color.RED);
+                secondsText.setForeground(Color.RED);
+                scoreNumText.setForeground(Color.RED);
+
+                scoreText.setFont(font);
+                timerText.setFont(font);
+                secondsText.setFont(font);
+                scoreNumText.setFont(font);
+
+                add(scoreText);
+                add(scoreNumText);
+                add(timerText);
+                add(secondsText);
+
+
+                scoreText.setAlignmentX(Component.CENTER_ALIGNMENT);
+                scoreNumText.setAlignmentX(Component.CENTER_ALIGNMENT);
+                timerText.setAlignmentX(Component.CENTER_ALIGNMENT);
+                secondsText.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+
+                scoreText.setAlignmentY(Component.TOP_ALIGNMENT);
+                scoreNumText.setAlignmentY(Component.TOP_ALIGNMENT);
+                timerText.setAlignmentY(Component.TOP_ALIGNMENT);
+                secondsText.setAlignmentY(Component.TOP_ALIGNMENT);
+
+
+
+                add(toStart);
+                toStart.setAlignmentX(Component.CENTER_ALIGNMENT);
+                header.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+
+                setContentPane(startMenu);
+
+                //create holes, add to specific locations
+                for (int i=0; i<9; i++){
+                    holeDesgin[i] = new JLabel("HOLE" + i);
+                    holeDesgin[i].setName("HOLE" + i);
+
+                    if(i==0){
+                        holeDesgin[i].setBounds(80,150,100,100);
+                    }
+                    if(i==1){
+                        holeDesgin[i].setBounds(280,150,100,100);
+                    }
+                    if(i==2){
+                        holeDesgin[i].setBounds(480,150,100,100);
+                    }
+                    if(i==3){
+                        holeDesgin[i].setBounds(80,300,100,100);
+                    }
+                    if(i==4){
+                        holeDesgin[i].setBounds(280,300,100,100);
+                    }
+                    if(i==5){
+                        holeDesgin[i].setBounds(480,300,100,100);
+                    }
+                    if(i==6){
+                        holeDesgin[i].setBounds(80,450,100,100);
+                    }
+                    if(i==7){
+                        holeDesgin[i].setBounds(280,450,100,100);
+                    }
+                    if(i==8){
+                        holeDesgin[i].setBounds(480,450,100,100);
+                    }
+                    add(holeDesgin[i]);
+                }
+
+            }
+        });
+
+        rules.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                //repaint screen
+                startMenu.removeAll();
+                revalidate();
+                repaint();
+
+                //set invisible spacing dimensions
+                Dimension minSize = new Dimension(10,600);
+                Dimension prefSize = new Dimension(10,600);
+                Dimension maxSize = new Dimension(Short.MAX_VALUE,600);
+
+
+                startMenu.setBackground(new Color(2,45,20));
+
+                //set header text
+                Font headFont = new Font(Font.SANS_SERIF,Font.BOLD,50);
+                JLabel header = new JLabel("Rules");
+                header.setForeground(new Color (0,0,0));
+                header.setFont(headFont);
+                add(header);
+
+                //////ADD TEXT HERE TO EXPLAIN RULES ETC////////
+
+
+                //add button
+                add(new Box.Filler(minSize,prefSize,maxSize));
+                add(toStart);
+                toStart.setAlignmentX(Component.CENTER_ALIGNMENT);
+                header.setAlignmentX(Component.CENTER_ALIGNMENT);
+                setContentPane(startMenu);
+
+
+
+
+
+            }
+        });
+
+        leaderboard.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //repaint screen
+                startMenu.removeAll();
+                revalidate();
+                repaint();
+
+                //set invisible spacing dimensions
+                Dimension minSize = new Dimension(10,600);
+                Dimension prefSize = new Dimension(10,600);
+                Dimension maxSize = new Dimension(Short.MAX_VALUE,600);
+
+                startMenu.setBackground(new Color(100,0,0));
+
+                //set header text
+                Font headFont = new Font(Font.SANS_SERIF,Font.BOLD,50);
+                JLabel header = new JLabel("Leaderboard");
+                header.setForeground(new Color (0,0,0));
+                header.setFont(headFont);
+                add(header);
+
+                //add button
+                add(new Box.Filler(minSize,prefSize,maxSize));
+                add(toStart);
+                toStart.setAlignmentX(Component.CENTER_ALIGNMENT);
+                header.setAlignmentX(Component.CENTER_ALIGNMENT);
+                setContentPane(startMenu);
+            }
+        });
+
+        quit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                System.exit(0);
+            }
+        });
+    }
 
     public static JLabel[] getHoleDesign() {
         return holeDesgin;
     }
 }
 
+
+
 class ClickGameBoard extends JPanel
 {
+
     private final class MouseDrag extends MouseAdapter {
         private boolean clickInCell = false;
         private Point last;
@@ -197,6 +317,7 @@ class ClickGameBoard extends JPanel
                 x = last.x;
                 y = last.y;
             }
+
             repaint();
         }
 
@@ -234,15 +355,15 @@ class ClickGameBoard extends JPanel
             // if any of the mouse inputs are within the coordinates of the cell
             if (array[i].getX() <= point.x && array[i].getY() <= point.y)
                 inCell = true;
+
         }
         return inCell;
     }
-
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
+
     }
 
-
-}
+    }
 
